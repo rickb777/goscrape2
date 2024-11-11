bc6a103809883b51ff46739a2f2248f786d515cf
Rick <1358735+rickb777@users.noreply.github.com>
2024-11-11 16:39:01 +0000

    another throttle tweak

--- download/throttle.go
+++ download/throttle.go
@@ -8,34 +8,20 @@ import (
 type Throttle int64
 
 const (
-	halfSecond = 500 * time.Millisecond
-	twoSeconds = 2 * time.Second
-	tenSeconds = 10 * time.Second
+	twoSeconds    = 2 * time.Second
+	thirtySeconds = 30 * time.Second
 )
 
 func (t *Throttle) SlowDown() {
 	a := (*int64)(t)
-	if !atomic.CompareAndSwapInt64(a, 0, int64(tenSeconds)) {
+	if !atomic.CompareAndSwapInt64(a, 0, int64(thirtySeconds)) {
 		atomic.AddInt64(a, int64(twoSeconds))
 	}
 }
 
 func (t *Throttle) SpeedUp() {
 	a := (*int64)(t)
-	for { // loop until altered
-		oldValue := atomic.LoadInt64(a)
-		if oldValue <= 0 {
-			return
-		}
-
-		newValue := oldValue - int64(halfSecond)
-		if newValue < 0 {
-			newValue = 0
-		}
-		if atomic.CompareAndSwapInt64(a, oldValue, newValue) {
-			return
-		}
-	}
+	atomic.StoreInt64(a, 0)
 }
 
 func (t *Throttle) IsNormal() bool {
---
+++ download/throttle_test.go
@@ -0,0 +1,24 @@
+package download
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestThrottle(t *testing.T) {
+	var throttle Throttle
+	assert.Equal(t, Throttle(0), throttle)
+
+	throttle.SlowDown()
+	assert.Equal(t, Throttle(thirtySeconds), throttle)
+
+	throttle.SlowDown()
+	assert.Equal(t, Throttle(32*time.Second), throttle)
+
+	throttle.SlowDown()
+	assert.Equal(t, Throttle(34*time.Second), throttle)
+
+	throttle.SpeedUp()
+	assert.Equal(t, Throttle(0), throttle)
+}
