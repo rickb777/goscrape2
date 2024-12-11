ad4db97122d96312288c9f8a197ca1bf6c327743
Rick <1358735+rickb777@users.noreply.github.com>
2024-12-11 11:38:09 +0000

    fixed query string in URL bug

--- mapping/file.go
+++ mapping/file.go
@@ -1,49 +1,117 @@
 package mapping
 
 import (
+	"fmt"
+	"github.com/rickb777/path"
 	"net/url"
-	"path"
+	"slices"
 	"strings"
 )
 
 const (
-	// HTMLExtension is the file extension that downloaded pages get.
-	HTMLExtension = ".html"
+	// htmlExtension is the file extension that downloaded pages get.
+	htmlExtension = ".html"
 
-	// PageDirIndex is the file name of the index file for every dir.
-	PageDirIndex = "index" + HTMLExtension
+	// pageDirIndex is the file name of the index file for every dir.
+	pageDirIndex = "index"
 )
 
 // GetFilePath returns a file path for a URL to store the URL content in.
 func GetFilePath(url *url.URL, isAPage bool) string {
 	trailingSlash := strings.HasSuffix(url.Path, "/")
-	if isAPage || trailingSlash {
-		fileName := GetPageFilePath(url)
-		return "." + fileName
+	if trailingSlash {
+		return "." + getHTMLPathWithTrailingSlash(url)
+	} else if isAPage {
+		return "." + getHTMLPathWithName(url)
 	} else {
-		return "." + url.Path
+		name, ext := path.SplitExt(url.Path)
+		return "." + name + prefixNonBlank(fileSafeQueryString(url.Query())) + ext
 	}
 }
 
 // GetPageFilePath returns a filename for a URL that represents a page.
 func GetPageFilePath(url *url.URL) string {
+	trailingSlash := strings.HasSuffix(url.Path, "/")
+	if trailingSlash {
+		return getHTMLPathWithTrailingSlash(url)
+	} else {
+		return getHTMLPathWithName(url)
+	}
+}
+
+func getHTMLPathWithTrailingSlash(url *url.URL) string {
 	fileName := url.Path
+	qs := fileSafeQueryString(url.Query())
 
 	switch {
 	case fileName == "":
-		fileName = "/" + PageDirIndex
-		// directory index will be index.html in the directory
+		fileName = "/" + pageDirIndex + prefixNonBlank(qs) + htmlExtension
 
-	case fileName[len(fileName)-1] == '/':
-		fileName += PageDirIndex
+	case qs == "":
+		fileName += pageDirIndex + htmlExtension
 
 	default:
-		ext := path.Ext(fileName)
+		fileName += qs + htmlExtension
+	}
+
+	return fileName
+}
+
+func getHTMLPathWithName(url *url.URL) string {
+	fileName := url.Path
+	qs := fileSafeQueryString(url.Query())
+
+	switch {
+	case fileName == "":
+		fileName = "/" + pageDirIndex + prefixNonBlank(qs) + htmlExtension
+		// directory index will be index.html in the directory
+
+	default:
+		name, ext := path.SplitExt(fileName)
 		// if file extension is missing add .html, otherwise keep the existing file extension
 		if ext == "" {
-			fileName += HTMLExtension
+			ext = htmlExtension
+		}
+		if name != "" {
+			fileName = name + prefixNonBlank(qs) + ext
+		} else {
+			fileName = qs + ext
 		}
 	}
 
 	return fileName
+}
+
+func fileSafeQueryString(values url.Values) string {
+	if len(values) == 0 {
+		return ""
+	}
+
+	nSegments := 0
+
+	var keys []string
+	keys = make([]string, 0, len(values))
+	for k, vs := range values {
+		keys = append(keys, k)
+		nSegments += len(vs)
+	}
+	slices.Sort(keys)
+
+	segments := make([]string, 0, nSegments)
+
+	for _, k := range keys {
+		vs := values[k]
+		for _, v := range vs {
+			segments = append(segments, fmt.Sprintf("%s=%s", k, v))
+		}
+	}
+
+	return strings.Join(segments, "_")
+}
+
+func prefixNonBlank(s string) string {
+	if s == "" {
+		return s
+	}
+	return "_" + s
 }
--- mapping/file_test.go
+++ mapping/file_test.go
@@ -4,7 +4,6 @@ import (
 	"io"
 	"log/slog"
 	urlpkg "net/url"
-	"os"
 	"testing"
 
 	"github.com/rickb777/goscrape2/logger"
@@ -17,15 +16,16 @@ func TestGetFilePath(t *testing.T) {
 		expectedFilePath string
 	}
 
-	pathSeparator := string(os.PathSeparator)
-
 	var cases = []filePathCase{
-		{downloadURL: "https://github.com/", expectedFilePath: "./index.html"},
 		{downloadURL: "https://github.com/#fragment", expectedFilePath: "./index.html"},
-		{downloadURL: "https://github.com/test", expectedFilePath: "./test.html"},
-		{downloadURL: "https://github.com/test/", expectedFilePath: "./test" + pathSeparator + "index.html"},
-		{downloadURL: "https://github.com/test.aspx", expectedFilePath: "./test.aspx"},
-		{downloadURL: "https://google.com/settings", expectedFilePath: "./settings.html"},
+		{downloadURL: "https://github.com/.abc#fragment", expectedFilePath: "./.abc"},
+		{downloadURL: "https://github.com/test#fragment", expectedFilePath: "./test.html"},
+		{downloadURL: "https://github.com/test/#fragment", expectedFilePath: "./test/index.html"},
+		{downloadURL: "https://github.com/test.aspx#fragment", expectedFilePath: "./test.aspx"},
+		{downloadURL: "https://github.com/test/info.aspx#fragment", expectedFilePath: "./test/info.aspx"},
+		{downloadURL: "https://github.com/test/info.aspx?a=1&b=2&b=3#fragment", expectedFilePath: "./test/info_a=1_b=2_b=3.aspx"},
+		{downloadURL: "https://google.com/settings?year=2006&month=11#fragment", expectedFilePath: "./settings_month=11_year=2006.html"},
+		{downloadURL: "https://google.com/settings/?year=2006&month=11#fragment", expectedFilePath: "./settings/month=11_year=2006.html"},
 	}
 
 	logger.Logger = slog.New(slog.NewTextHandler(io.Discard, nil))
